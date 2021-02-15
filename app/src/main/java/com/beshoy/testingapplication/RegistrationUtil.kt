979f27f953247e665f521c8711f2c147c411d808
beshoyfakhry <beshoyfakhry@gmail.com>
2021-02-15 15:57:08 +0200

    update the gradles files

--- app/src/main/java/com/beshoy/testingapplication/RegistrationUtil.kt
+++ app/src/main/java/com/beshoy/testingapplication/RegistrationUtil.kt
@@ -1,4 +1,18 @@
 package com.beshoy.testingapplication
 
 object RegistrationUtil {
+
+
+    /*
+    * user
+    * password
+    * */
+private val existingUsers = listOf("Loli","Perla")
+    fun validateRegistrationInput(
+        username: String,
+        password: String,
+        confirmationPassword: String
+    ): Boolean {
+        return true
+    }
 }

9d2a7b8c852ff4d11739d0a2151f47761874933c
beshoyfakhry <beshoyfakhry@gmail.com>
2021-02-16 11:21:49 +0200

    Adding singleton usage comment

--- app/src/main/java/com/beshoy/testingapplication/data/local/ShoppingItemDatabase.kt
+++ app/src/main/java/com/beshoy/testingapplication/data/local/ShoppingItemDatabase.kt
@@ -3,6 +3,9 @@ package com.beshoy.testingapplication.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+/*
+* Why we did not use the singleton pattern here , because we are using DI
+* */
 @Database(entities = [ShoppingItem::class], version = 1)
 
 abstract class ShoppingItemDatabase : RoomDatabase() {
